b7dbba59a49eeb4e0244e43f505760120aa55330
Sofy Korzhova <sofykorzhova@gmail.com>
2021-08-11 13:58:30 +0300

    Updated package json-request-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -3,7 +3,7 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle::class => ['all' => true],
-    Symfona\JsonRequestBundle\JsonRequestBundle::class => ['all' => true],
+    Symfona\Bundle\JsonRequestBundle\JsonRequestBundle::class => ['all' => true],
     Symfona\JsonResponseBundle\JsonResponseBundle::class => ['all' => true],
     Symfona\PaginationBundle\PaginationBundle::class => ['all' => true],
     Symfona\Pagination\Doctrine\MongoDB\AdapterBundle\AdapterBundle::class => ['all' => true],
